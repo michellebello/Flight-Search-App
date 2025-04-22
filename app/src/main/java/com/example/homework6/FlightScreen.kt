f23ede720d5fc38781705bd697b3ae786bcf273f
michellebello <michellebelloveramendi@gmail.com>
2025-04-22 15:39:09 -0400

    "started the flight search app"

---
+++ app/src/main/java/com/example/homework6/FlightScreen.kt
@@ -0,0 +1,51 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.homework6
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+val blueColor = Color(0XFF637ec7)
+val ivoryColor = Color(0XFFf6f7f0)
+
+@Composable
+fun FlightScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = blueColor,
+                    titleContentColor = ivoryColor
+                ),
+                title = {
+                    Text("Flight Search")
+                }
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            contentPadding = innerPadding,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(20) { index ->
+                Text(
+                    text = "Item #$index",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/homework6/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.homework6
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.homework6.ui.theme.Homework6Theme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Homework6Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    FlightScreen()
+                }
+            }
+        }
+    }
+}
+
